8335f900bdcec50631102b37363b993128223f0c
Wikzo <Linkthewindwaker@gmail.com>
2014-06-18 18:17:46 +0200

    sunglasses animation started

---
+++ Game/Assets/Scripts/SunGlasses.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunGlasses : MonoBehaviour
+{
+    public Texture2D Black;
+
+    byte alpha = 190;
+
+    bool toggleGlasses = false;
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            toggleGlasses = !toggleGlasses;
+            //anim.Play
+        }
+    }
+
+    void OnGUI()
+    {
+        if (toggleGlasses)
+        {
+            GUI.color = new Color32(255, 255, 255, alpha);
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), Black, ScaleMode.StretchToFill);
+        }
+    }
+}
