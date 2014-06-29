136466cbafc52b270e1f7065db8bad8aff1c4ec0
Wikzo <Linkthewindwaker@gmail.com>
2014-06-29 15:38:22 +0200

    Follow path

--- Week2/Game/Assets/Code/PathDefinition.cs
+++ Week2/Game/Assets/Code/PathDefinition.cs
@@ -5,22 +5,78 @@ using System;
 
 public class PathDefinition : MonoBehaviour
 {
-    public Transform[] Points;
+    //public Transform[] Points; // old array
 
-    public IEnumerator<Transform> GetPathsEnumerator()
+    private List<Transform> targetPoints;
+
+    public IEnumerator<Transform> GetPathEnumerator() // ping-pong pattern
     {
-        throw new NotImplementedException();
+        // needs 1 point to go to target
+        if (targetPoints.Count < 1 || targetPoints == null)
+            yield break; // terminate sequence
+
+        var direction = 1; // forward/backward
+        var index = 0;
+
+        while (true)
+        {
+            yield return targetPoints[index]; // yield = give control back to the caller
+
+            if (targetPoints.Count == 1)
+                continue;
+
+            if (index <= 0)
+                direction = 1; // forward
+            else if (index >= targetPoints.Count - 1)
+                direction = -1; // backward
+
+            index = index + direction;
+        }
+    }
+
+    /*void GetTargetsBasedOnChildren() // array - old
+    {
+        // doesn't work, since array has to be initialized for FollowPath to work
+
+        int counter = 0;
+        foreach (Transform child in transform)
+            counter++;
+
+        Points = new Transform[counter];
+
+        int index = 0;
+
+        foreach (Transform child in transform)
+            Points[index++] = child;
+    }*/
+
+    void GetPointsBasedOnChildObjects()
+    {
+        //Debug.Log("Getting list of children");
+
+        targetPoints = new List<Transform>();
+        foreach (Transform child in transform)
+            targetPoints.Add(child);
+    }
+
+    void Awake()
+    {
+        GetPointsBasedOnChildObjects();
     }
 
     // debug draw in scene view
     public void OnDrawGizmos() // or OnDrawGizmosSelected()
     {
-        if (Points.Length < 2 || Points == null)
-            return;
+        //if (!Application.isPlaying)
+        GetPointsBasedOnChildObjects();
 
-        for (var i = 1; i < Points.Length; i++)
+        // needs 2 points to draw line
+        /*if (Points.Length < 2 || Points == null)
+            return;
+        */
+        for (var i = 1; i < targetPoints.Count; i++)
         {
-            Gizmos.DrawLine(Points[i - 1].position, Points[i].position);
+            Gizmos.DrawLine(targetPoints[i - 1].position, targetPoints[i].position);
         }
     }
 }
---
+++ Week2/Game/Assets/Code/SmoothDampExample.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// http://docs.unity3d.com/ScriptReference/Vector3.SmoothDamp.html
+
+public class SmoothDampExample : MonoBehaviour {
+
+	public Transform target;
+	public float smoothTime = 0.3F;
+	public Vector3 velocity = Vector3.zero;
+	void Update()
+	{
+		//Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
+		transform.position = Vector3.SmoothDamp(transform.position, target.position, ref velocity, smoothTime);
+	}
+}
