75006b51cfbadaedd95ed9e52a9a5f8c0eb88b9b
Wikzo <Linkthewindwaker@gmail.com>
2014-06-29 13:30:37 +0200

    Sprites

---
+++ Week2/Game/Assets/Code/PathDefinition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class PathDefinition : MonoBehaviour
+{
+    public Transform[] Points;
+
+    public IEnumerator<Transform> GetPathsEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    // debug draw in scene view
+    public void OnDrawGizmos() // or OnDrawGizmosSelected()
+    {
+        if (Points.Length < 2 || Points == null)
+            return;
+
+        for (var i = 1; i < Points.Length; i++)
+        {
+            Gizmos.DrawLine(Points[i - 1].position, Points[i].position);
+        }
+    }
+}
